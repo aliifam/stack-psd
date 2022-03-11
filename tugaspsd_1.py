c66bb683556d89c9d14807dc9b1baa57bfbcead2
Aliif Arief <aliif.arief.m@gmail.com>
2022-03-11 15:50:16 +0700

    add: some method

--- tugaspsd_1.py
+++ tugaspsd_1.py
@@ -1,23 +1,36 @@
 class Node():
     def __init__(self, data): #constructor
         self.data = data
+        self.next = None
 
-class stack():
+class linkstack():
     def __init__(self):
         self.head = None
         self.tail = None
-
-    def addTail(self, data):
-        databaru = Node(data)
-        if self.head == None:
-            self.head = databaru
-            self.tail = databaru
-        else:
-            self.tail.next = databaru
-            self.tail = databaru
     
-    def display(self):
-        pass
+    def addHead(self, data):
+        newdata = Node(data)
+        if self.head == None:
+            self.head = newdata
+            self.head = newdata
+        else:
+            newdata.next = self.head
+            head = newdata
+    
+    def removeHead(self):
+        if self.head == None:
+            print("error stack sudah kosong")
+        elif self.head.next == None:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+    
+    def peekHead(self):
+        if self.head == None:
+            return "error stack sudah kosong"
+        else:
+            return self.head.data
     
 
 def main():
@@ -28,3 +41,15 @@ def main():
     
 
 main()
+
+"""
+untuk membalik angka maka gunakan stack,
+dengan konsepnya yang LIFO maka bisa dilakukan
+push data -> peek data -> pop data, terus menerus
+hingga data pada stack kosong,
+kali ini method yang digunakan :
+
+- addhead = push
+- removehead = pop
+- peekhead = peek
+"""
