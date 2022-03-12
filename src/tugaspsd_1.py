41874938e3190ee4241715ccb9e02643f07c2c58
Aliif Arief <aliif.arief.m@gmail.com>
2022-03-12 14:03:56 +0700

    finishing

--- src/tugaspsd_1.py
+++ src/tugaspsd_1.py
@@ -31,18 +31,7 @@ class linkstack():
             return "error stack sudah kosong"
         else:
             return self.head.data
-    
-
-def func_test(numbers_list):
-    stacked_list = linkstack()
-    reversed_numbers = []
-    for i in numbers_list:
-        stacked_list.pushData(i)
-    for j in range(len(numbers_list)):
-        reversed_numbers.append(stacked_list.peekData())
-        stacked_list.popData()
         
-
 def main():
 
     list_angka = input().split(" ")
